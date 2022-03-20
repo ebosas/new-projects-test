60bf5003b7a5ae6e789232be1dc4a3f13fcf26b4
ebosas <ebosas>
2022-03-20 07:57:56 +0000

    Add new model

--- models/new-dbt.sql
+++ models/new-dbt.sql
@@ -1,1 +1,19 @@
-select * from promethium.customer
+{{config(
+	schema = "promethium",
+	materialized = "table"
+)}}
+WITH select_step1 as (
+  SELECT
+    "customer"."id" AS "id",
+    "customer"."name" AS "name",
+    "customer"."city" AS "city",
+    "customer"."industry_type" AS "industry_type"
+  FROM
+    "promethium"."customer"
+)
+SELECT
+  *
+FROM
+  select_step1
+LIMIT
+  100
